fd871cc350e8f2f54056c4a6cc005d86299617cc
Henrique Adonai <henriqueadonai@gmail.com>
2017-05-28 23:51:37 -0400

    Add presidentes

--- src/App.js
+++ src/App.js
@@ -2,28 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'tachyons';
+import ListaPresidente from './ListaPresidentes';
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      presidentes : []
+    }
+
+    presidentes : [
+      {id:1, nome:'Tancredo Neves', foto:'./img/Presidentes/TancredoNeves.jpg',mandato:'Não Assumiu'},
+      {id:2, nome:'Sarney' , foto:'./img/Presidentes/Sarney.jpg',mandato:'1985-1990'}, 
+      {id:3, nome:'Fernando Collor' , foto:'./img/Presidentes/Fernando_collor.jpg',mandato:'1990-1992'},
+      {id:4, nome:'Itamar Franco' , foto:'./img/Presidentes/ItamarFranco.jpg',mandato:'1992-1994'},
+      {id:5, nome:'Fernando Henrique' , foto:'./img/Presidentes/FernandoHenrique.jpg',mandato:'1994-2002'},
+      {id:6, nome:'Lula', foto:'',mandato:'./img/Presidentes/Lula.jpg', mandato:'2002-2010'},
+      {id:7, nome:'Dima', foto:'',mandato:'./img/Presidentes/Dilma.jpg', mandato:'2011-2016'},
+      {id:7, nome:'Temer', foto:'',mandato:'./img/Presidentes/Temer.jpg', mandato:'Atualidade'}
+    ]
+  }
+
   render() {
     return (
       <div>
 
-<div className="flex justify-left pa4 ">
-  <h1 className="mt0 titlecolor courier">Fora Todo Mundo! Renovação!</h1>
-</div>
+        {/*<div className="flex justify-left pa4 ">
+          <h1 className="mt0 titlecolor courier">Fora Todo Mundo! Renovação!</h1>
+        </div>*/}
+        <header className="bg-white black-80 tc pv4 avenir">
+          <a href="" className="bg-black-80 ba b--black dib pa3 w2 h2 br-100">
+              <title>Fora todo mundo</title>
+              
+          </a>
+          <h1 className="mt2 mb0 baskerville i fw1 f1">Fora todo mundo</h1>
+          <h2 className="mt2 mb0 f6 fw4 ttu tracked">Tempo de mudança e oportunidades</h2>
+          <nav className="bt bb tc mw7 center mt4">
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/">Home</a>
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Centro</a>
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" >Direita</a>
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-light-pink dib pa3 ph4-l" >Esquerda</a>
+            <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" >Contato</a>
+          </nav>
+        </header>
 
-{/*mw7-ns center pa3 ph5-ns*/}
-<section >  
-<img src={require('./img/brasilia.jpg')} />
-</section>
+        <ListaPresidente presidentes={this.state.presidentes}/>
+ 
 
-<footer className="pv4 ph3 ph5-m ph6-l mid-gray">
-  <small className="f6 db tc">© 2016 <b className="ttu">Fora Todo Mundo</b> - Open Source Project</small>
-  <div className="tc mt3">
-    <a href="https://github.com/henriqueadonai/foratodomundo" title="GitHub" className="f6 dib ph2 link mid-gray dim">GitHub</a>
-  </div>
-</footer>
+
+        <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+          <small className="f6 db tc">© 2016 <b className="ttu">Fora Todo Mundo</b> - Open Source Project</small>
+          <div className="tc mt3">
+            <a href="https://github.com/henriqueadonai/foratodomundo" title="GitHub" className="f6 dib ph2 link mid-gray dim">GitHub</a>
+          </div>
+        </footer>
 
       </div>
     );
