d3737dae9f3cc403b0c44012da372c2aa4bc6899
Henrique Adonai <henriqueadonai@gmail.com>
2017-06-19 23:10:28 -0400

    Correcao de ID

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 import 'tachyons';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-100774275-1');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
+ReactGA.initialize('UA-100774275-1');
 
